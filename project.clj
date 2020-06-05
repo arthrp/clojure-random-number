06fb3be47acc6a3635796879215690c84f135d80
Arthur <arthrp@users.noreply.github.com>
2020-06-05 20:52:27 +0300

    Adding description

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject random-number "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Guess random number from 1 to 10"
+  :url "https://github.com/arthrp/clojure-random-number"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.1"]]
